470db45c895c4e66f00eaa095f63568b39dc24ec
m <kukhmax@gmail.com>
2025-08-16 18:22:27 +0200

    feat(deploy): remove all custom deployment configurations

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,9 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 from app.models import *  # Импорт всех моделей для Base
-from .db_utils import create_async_engine_with_ssl_context 
 
-engine = create_async_engine_with_ssl_context(settings.DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
